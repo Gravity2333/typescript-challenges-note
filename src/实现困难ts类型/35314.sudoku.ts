0325b6dc935511591b7fd596c44d57519fc24422
liuze <liuze@machloop.com>
2025-09-03 14:44:09 +0800

    fix

--- src/实现困难ts类型/35314.sudoku.ts
+++ src/实现困难ts类型/35314.sudoku.ts
@@ -75,12 +75,9 @@ type ReverseMatric<
     >
   : R;
 
-// type Revered = ReverseMatric<Matrix>;
-
 type BuildTuple<N extends number, R extends any[] = []> = R["length"] extends N
   ? R
   : BuildTuple<N, [...R, 1]>;
-
 
 type Plus<
   A extends number,
@@ -90,59 +87,89 @@ type Plus<
 > = [...ATuple, ...BTuple]["length"];
 
 type LessThan<A extends number, B extends number> = BuildTuple<B> extends [
-    ...BuildTuple<A>,
-    ...any[]
-  ]
-    ? true
-    : false;
+  ...BuildTuple<A>,
+  ...any[]
+]
+  ? true
+  : false;
 
-    
 type BuildRange<
-F extends number ,
-T extends number, 
-C extends number = F,
-R extends number[] = []> =
-LessThan<Plus<C,1>&number,T> extends true ? BuildRange<F,T,Plus<C,1>&number,[...R,C]>: R[number]
+  F extends number,
+  T extends number,
+  C extends number = F,
+  R extends number[] = []
+> = LessThan<Plus<C, 1> & number, T> extends true
+  ? BuildRange<F, T, Plus<C, 1> & number, [...R, C]>
+  : R[number];
 
-type FlattenObj<O extends Record<string,any>> = O[keyof O]
+type FlattenObj<O extends Record<string, any>> = O[keyof O];
 
+type UnionToIntersection<U extends any> = (
+  U extends any ? (arg: U) => void : never
+) extends (arg: infer I) => void
+  ? I
+  : never;
+
+type LastUnion<U extends any> = UnionToIntersection<
+  U extends any ? (arg: U) => void : never
+> extends (arg: infer LastUnion) => void
+  ? LastUnion
+  : never;
+
+type UnionToArray<U extends any> = [U] extends [never]
+  ? []
+  : LastUnion<U> extends infer L
+  ? [...UnionToArray<Exclude<U, L>>, L]
+  : [];
 
 type ExtractBlock<
   Matric extends number[][],
   X1 extends number,
   X2 extends number,
   Y1 extends number,
-  Y2 extends number,
-> = FlattenObj<FlattenObj<{
-    [r in BuildRange<X1, X2>]: Matric[r] extends infer ColumnResults extends number[]
-      ? { [c in BuildRange<Y1, Y2>]: ColumnResults[c] }
-      : never
-  }>>;
+  Y2 extends number
+> = UnionToArray<
+  FlattenObj<
+    FlattenObj<{
+      [r in BuildRange<
+        X1,
+        X2
+      >]: Matric[r] extends infer ColumnResults extends number[]
+        ? { [c in BuildRange<Y1, Y2>]: ColumnResults[c] }
+        : never;
+    }>
+  >
+>;
 
-
-
-// type TransFormSubMatrix<
-// Matric extends number[][],
-// Len extends number = Matric['length'],
-// X extends number = 0,
-// Y extends number = 0,
-// R extends any[] = [],
-// > = X extends Len ? Y extends Len ? R :
-// X extends Len ? TransFormSubMatrix<Matric,Len,Y,Plus<Y,3>&number,[...R,
-//     [ExtractBlock<Matric,X,Plus<X,3>&number,Y,Plus<Y,3>&number>]
-// ]>:Y extends Len ? TransFormSubMatrix<Matric,Len,0,Plus<Y,3>&number,[...R,
-//     [ExtractBlock<Matric,X,Plus<X,3>&number,Y,Plus<Y,3>&number>]
-// ]>
-
+type TransFormSubMatrix<
+  Matric extends number[][],
+  Len extends number = Matric["length"],
+  X extends number = 0,
+  Y extends number = 0,
+  R extends any[] = []
+> = X extends Len
+  ? Y extends Len
+    ? R
+    : TransFormSubMatrix<Matric, Len, 0, Plus<Y, 3> & number, R>
+  : TransFormSubMatrix<
+      Matric,
+      Len,
+      Plus<X, 3> & number,
+      Y,
+      [
+        ...R,
+        ExtractBlock<Matric, X, Plus<X, 3> & number, Y, Plus<Y, 3> & number>
+      ]
+    >;
 
 type ValidSudoku<Matric extends number[][]> =
   ValidateSudoKuRows<Matric> extends true
     ? ValidateSudoKuRows<ReverseMatric<Matric>> extends true
-      ? true
+      ? ValidateSudoKuRows<TransFormSubMatrix<Matrix>> extends true
+        ? true
+        : false
       : false
     : false;
-
-
 
 type result = ValidSudoku<Matrix>; // expected to be true
 
